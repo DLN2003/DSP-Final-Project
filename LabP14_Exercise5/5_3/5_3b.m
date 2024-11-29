60b894f5d3baada576f8bc9dab181d6df782a977
Zylo-Shadows <giovanniagut@gmail.com>
2024-11-29 16:41:47 -0700

    Create 5_3b.m

---
+++ LabP14_Exercise5/5_3/5_3b.m
@@ -0,0 +1,27 @@
+%% 5.3b)
+% Frequency bands for 5 BPF
+bands = [BP_Filters(2, "StartingFreq_Hz_"), BP_Filters(2, "EndingFreq_Hz_"); 
+        BP_Filters(3, "StartingFreq_Hz_"), BP_Filters(3, "EndingFreq_Hz_"); 
+        BP_Filters(4, "StartingFreq_Hz_"), BP_Filters(4, "EndingFreq_Hz_");
+        BP_Filters(5, "StartingFreq_Hz_"), BP_Filters(5, "EndingFreq_Hz_");
+        BP_Filters(6, "StartingFreq_Hz_"), BP_Filters(6, "EndingFreq_Hz_");];
+L = 101;                 % Filter length
+N = 1024;                % FFT size for frequency response
+numBands = size(bands, 1);
+filterOutputs = zeros(numBands, length(xx)); % To store filter outputs
+
+for i = 1:numBands
+    % Calculate center frequency and normalized cutoff
+    fc = (bands{i, "StartingFreq_Hz_"} + bands{i, "EndingFreq_Hz_"}) / 2;
+    wc = 2 * pi * fc / fs; % Convert to normalized frequency
+    
+    % Call HammingNorm to generate frequency response
+    H = HammingNorm(wc, L, N); % Obtain frequency response
+    
+    % Apply the filter (IFFT to create time-domain filter coefficients)
+    h = ifft(ifftshift(H), 'symmetric');
+    h = h(1:L); % Trim to filter length
+    
+    % Filter the signal
+    filterOutputs(i, :) = filter(h, 1, xx);
+end
