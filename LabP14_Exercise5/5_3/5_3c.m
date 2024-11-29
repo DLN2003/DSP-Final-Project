32b495803be70ee9d8ace23ab33b4656a3804d3b
Zylo-Shadows <giovanniagut@gmail.com>
2024-11-29 16:42:19 -0700

    Create 5_3c.m

---
+++ LabP14_Exercise5/5_3/5_3c.m
@@ -0,0 +1,12 @@
+%% 5.3c)
+% Plot Frequency Responses for All Filters
+figure(11);
+for i = 1:numBands
+    subplot(numBands, 1, i);
+    plot(t, filterOutputs(i, :));
+    title(['Filter Output for Band ', num2str(round(bands{i, "StartingFreq_Hz_"})), ...
+        '-', num2str(round(bands{i, "EndingFreq_Hz_"})), ' Hz']);
+    xlabel('Time (s)');
+    ylabel('Amplitude');
+    grid on;
+end
