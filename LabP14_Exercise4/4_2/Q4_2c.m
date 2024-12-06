7d281859e1f0bc65f17275bb360cfdf56c236716
Zylo-Shadows <giovanniagut@gmail.com>
2024-12-06 12:50:08 -0700

    Added Documentation

--- LabP14_Exercise4/4_2/Q4_2c.m
+++ LabP14_Exercise4/4_2/Q4_2c.m
@@ -1,3 +1,17 @@
+% Authored By: Giovanni Gutierrez
+%
+% Last Modified: Nov. 2024
+%
+% Reference 4 Lab Exercises: 4.2 A Better BPF
+%
+% Using the output from the magnitude and frequency 
+% responses at specific values of omega in 4.2a), we  
+% demonstrate how an input signal transforms using  
+% the 41-length filter. This code displays the
+% handwritten calculation of an output response.
+%
+
+
 %% 4.2c) Given a specific input, determine the output signal by hand
 
 % Display image of hand written derivation of output as requested by the problem.
